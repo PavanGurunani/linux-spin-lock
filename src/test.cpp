bc45641694a11b2dc7227132a5fe441a1bbe830d
Pavan Gurunani <gurunani.pavan@sasken.com>
2020-02-10 12:40:45 +0530

    adding new files

--- src/test.cpp
+++ src/test.cpp
@@ -7,11 +7,17 @@ using namespace std;
 int main() 
 {
     int x =10;
+
+    // ref is a reference to x.
     int& ref = x;
 
     cout << "value of x="<<x << " and ref=" << ref << endl;
+
+    // Value of x is now changed to 20
     ref = 20;
     cout << "value of x="<<x << " and ref=" << ref << endl;
+
+    // Value of x is now changed to 30
     x =30;
     cout << "value of x="<<x << " and ref=" << ref << endl;
 

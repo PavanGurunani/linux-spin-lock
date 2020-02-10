f15c6768fa6d52f9c9f00974b32098ac89344d84
Pavan Gurunani <gurunani.pavan@sasken.com>
2020-02-10 16:03:45 +0530

    adding new files

--- src/refrence_001.cpp
+++ src/refrence_001.cpp
@@ -1,17 +1,30 @@
-// C++ program to demonstrate 
-// Author: Pavan Gurunani
+/* C++ program to demonstrate
+   Author: Pavan Gurunani
+
+   Reference Variable : When a variable is declared as reference,
+   it becomes an alternative name for an existing variable.
+   A variable can be declared as reference by putting ‘&’ in the declaration.
+
+*/
 #include <iostream> 
   
 using namespace std; 
   
 int main() 
 {
-    int x =10;
+    int x =10,y=20;
 
     // ref is a reference to x.
     int& ref = x;
 
+    //int& ref = y;   //Not allowed :  Once a reference is created,
+                      //it cannot be later made to reference another object;
+                      //it cannot be reseated. This is often done with pointers.
+
+    int& ref1 = x;
+
     cout << "value of x="<<x << " and ref=" << ref << endl;
+    cout << "value of x="<<x << " and ref1=" << ref1 << endl;
 
     // Value of x is now changed to 20
     ref = 20;
